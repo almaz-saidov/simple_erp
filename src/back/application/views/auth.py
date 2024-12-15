c729ce54d50076862bce188fd213c005dfd16fbd
Almaz Saidov <almaz.saidov.03@bk.ru>
2024-12-15 16:16:23 +0300

    try auth without /api

--- src/back/application/views/auth.py
+++ src/back/application/views/auth.py
@@ -4,7 +4,7 @@ from application.utils import init_data_checker
 from application import app
 
 
-@app.route('/api/auth', methods=['GET', 'POST'])
+@app.route('/auth', methods=['GET', 'POST'])
 @init_data_checker
 def telegram_auth():
     return jsonify({'status': 'authorized'}), 200 # cool

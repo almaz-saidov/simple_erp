d9e35010a30cf59de58a5b9e155c05354871ef4e
Almaz Saidov <almaz.saidov.03@bk.ru>
2024-12-16 18:16:15 +0300

    try get fckng initData from data blyat

--- src/back/application/utils/checker.py
+++ src/back/application/utils/checker.py
@@ -10,7 +10,7 @@ from config.config import settings
 def init_data_checker(func):
     @wraps(func)
     def wrapper(*args, **kwargs):
-        init_data = request.get_json().data.get('initData')
+        init_data = request.get_json().get('initData')
         print(f'\n\n\n{init_data}\n\n\n')
         if not init_data:
             return jsonify({'error': 'No initData'}), 400

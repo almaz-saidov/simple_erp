ce6340048be0dcb067a99fa0574b19016e4c9c99
Almaz Saidov <almaz.saidov.03@bk.ru>
2024-12-17 20:17:17 +0300

    try to set cookie as it comes

--- src/back/application/views/auth.py
+++ src/back/application/views/auth.py
@@ -1,6 +1,5 @@
 from flask import jsonify, make_response, request
 
-from application.utils.init_data import TelegramInitData
 from application.utils import initial_init_data_checker
 from application import app
 
@@ -8,9 +7,8 @@ from application import app
 @app.route('/api/auth', methods=['GET', 'POST'])
 @initial_init_data_checker
 def telegram_auth():
-    init_data = TelegramInitData(request.get_json().get('initData'))
     response = make_response('Cookie was set!')
-    response.set_cookie('initData', init_data)
+    response.set_cookie('initData', request.get_json().get('initData'))
     response.status_code = 200
     # return jsonify({'status': 'authorized'}), 200 # cool
     return response

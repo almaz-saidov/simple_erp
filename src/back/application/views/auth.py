f98b7087d6b3efd54af5819072bd451ddd45ca9b
Almaz Saidov <almaz.saidov.03@bk.ru>
2025-01-13 20:07:53 +0300

    iiiuuu

--- src/back/application/views/auth.py
+++ src/back/application/views/auth.py
@@ -9,7 +9,7 @@ from application.utils.init_data import TelegramInitData
 @app.route('/api/auth', methods=['GET', 'POST'])
 @initial_init_data_checker
 def telegram_auth():
-    telegram_data = TelegramInitData(request.cookies.get('initData'))
+    telegram_data = TelegramInitData(request.get_json().get('initData'))
     user_data = telegram_data.to_dict().get('user')
     user_id = user_data.get('id')
     

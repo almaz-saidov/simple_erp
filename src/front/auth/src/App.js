8c24f8d55144900229bb65615218a13456f1825c
adel <ayedel123@gmail.com>
2025-04-01 21:26:57 +0300

    auth

--- src/front/auth/src/App.js
+++ src/front/auth/src/App.js
@@ -1,98 +1,128 @@
-import { useEffect, useState, Fragment } from 'react';
+import React, { useEffect, useState, Fragment } from 'react';
 import { SyncLoader } from 'react-spinners';
 import { ReactComponent as FailIcon } from './assets/auth_fail_icon.svg';
 import { useNavigate } from 'react-router-dom';
 import { retrieveLaunchParams } from "@telegram-apps/sdk";
+import toast, { Toaster, useToasterStore } from "react-hot-toast";
 
-import './styles/App.css';
+
+import gif from '../src/assets/1.gif';
 import "./styles/LoaderWrapper.css";
 
+import styles from './styles/App.module.css';
+
 function App() {
+
+    const [login, setLogin] = useState('');
+    const [password, setPassword] = useState('');
     const [loading, setLoading] = useState(true);
     const [timeoutReached, setTimeoutReached] = useState(false);
     const [isAuthorized, setIsAuthorized] = useState(false);
+    const [state, setState] = useState('idol');
     const navigate = useNavigate();
-
-
-    useEffect(() => {
-        const timeout = setTimeout(() => {
-            setTimeoutReached(true);
-            setLoading(false);
-        }, 3000); // Задержка на 3 секунды
-
-        try {
-            const csrfToken = "almaz-ymeet-delat-graz";
-            const init_data = retrieveLaunchParams().initDataRaw;
-            //console.log(init_data);
-
-            const initializeAuth = async (postUrl) => {
-                try {
-
-                    //console.log("RESPONSE__TRY");
-                    const response = await fetch(postUrl, {
-                        method: 'POST',
-                        headers: {
-                            'Content-Type': 'application/json',
-                            'Token': csrfToken,
-                        },
-                        body: JSON.stringify(
-                            {
-                                initData: init_data,
-                            }
-                        ),
-                    });
-                    //console.log("RESPONSE__1", response);
-                    if (!response.ok) {
-                        throw new Error('Network response was not ok');
-                    }
-                    const data = await response.json();
-                    console.log("RESPONSE_DATA", data);
-                    //console.log("USER_STATUS", data.user_status);
-                    localStorage.setItem('user_status', data.user_status);
-                    setIsAuthorized(true);
-                    // Теперь этот редирект будет происходить в следующем useEffect 
-                    timeoutReached && setLoading(false); // Обновляем состояние загрузки
-                    return; // Выходим из функции
-                } catch (error) {
-                    timeoutReached && setLoading(false);
-                    console.error('Ошибка при авторизации:', error);
-                }
-            };
-
-            initializeAuth('https://asm3ceps.ru/api/auth');
-
-        } catch (e) {
-            timeoutReached && setLoading(false);
-            console.log("Error", e.message);
-        }
-
-        return () => clearTimeout(timeout); // Очистка таймера при размонтировании
-    }, []);
 
     useEffect(() => {
         if (!loading && timeoutReached && isAuthorized) {
-            navigate('/front');
+            navigate('/front/test');
             window.location.reload();
         }
     }, [loading, timeoutReached, navigate]);
 
+    const handleLogin = () => {
+        if (!password || !login || password.length === 0 || login.length === 0) {
+            toast.error('Заполните все поля');
+            return;
+        }
+
+        setState('loading');
+
+        setTimeout(() => {
+            fetch(`http://127.0.0.1/auth?login=${login}&password=${password}`)
+                .then(res => {
+                    setState('idol');
+                    toast.success('URA');
+                    navigate('/front');
+                    window.location.reload();
+                })
+                .catch(e => {
+                    setState('error');
+                    toast.error(e.message);
+                });
+        }, 1500);
+    };
+
     return (
-        <div className="App">
-            {loading || isAuthorized ? (
-                <div className='VideoLoaderWrapper'>
-                    <div className='VideoWrapper'>
-                        <img className="LoaderGif" src="/static/1.gif" alt="я джифка"></img>
+        <div className={styles.App}>
+            {state === 'idol' ?
+                <div className={styles.content}>
+                    <h1 className={styles.authHeader}>MG Parts</h1>
+                    <div className={styles.inputWrapper}>
+                        <label htmlFor="loginInput">Login</label>
+                        <input
+                            id="loginInput"
+                            name="login"
+                            type="text"
+                            value={login}
+                            onChange={(e) => { setLogin(e.target.value) }}
+                            className={styles.input}
+                            required
+                        />
+
+                        <label htmlFor="passwordInput">Password</label>
+                        <input
+                            id="passwordInput"
+                            name="password"
+                            type="password"
+                            value={password}
+                            onChange={(e) => { setPassword(e.target.value) }}
+                            className={styles.input}
+                            required
+                        />
                     </div>
-                    <span>СОБИРАЕМ ДЕТАЛИ...</span>
+                    <button type="submit" id="submitButton" className={styles.loginButton} onClick={handleLogin}>Login</button>
                 </div>
-            ) : (
-                <Fragment>
-                    <FailIcon className='FailAuthIcon' />
-                    <h1 className='FailAuthMessage'>Unauthorized Access</h1>
-                </Fragment>
-            )}
-        </div>
+                : state === 'loading' ? (
+                    <div className={styles.VideoLoaderWrapper}>
+                        <div className={styles.VideoWrapper}>
+                            <img className={styles.LoaderGif} src={gif} alt="я джифка"></img>
+                        </div>
+                        <span>СОБИРАЕМ ДЕТАЛИ...</span>
+                    </div>
+                ) : (
+                    <Fragment >
+                        <FailIcon className={styles.FailAuthIcon} />
+                        <h1 className={styles.FailAuthMessage}>Unauthorized Access</h1>
+                    </Fragment>
+                )}
+            <ToasterWithMax toastOptions={{
+                duration: 1000,
+                style: {
+                    backgroundColor: '#131313',
+                    color: '#DBDBDB',
+                }
+            }} />
+        </div >
+
     );
 }
 
 export default App;
+
+
+function useMaxToasts(max) {
+    const { toasts } = useToasterStore();
+
+    useEffect(() => {
+        toasts
+            .filter((t) => t.visible)
+            .filter((_, i) => i >= max)
+            .forEach((t) => toast.dismiss(t.id));
+    }, [toasts, max]);
+}
+
+export function ToasterWithMax(props) {
+    const { max = 1, ...restProps } = props;
+    useMaxToasts(max);
+
+    return <Toaster {...restProps} />;
+}
